ea8b70f2131c61d71d58aa15bc958bd005b3c587
cmmgw <cecily.martin@gmail.com>
2021-01-23 22:00:46 -0500

    Adding schema.sql file

--- schema.sql
+++ schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE employees (
 
 -- Department Managers Table
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -46,17 +46,27 @@ CREATE TABLE titles (
   title VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 -- Department Employees Table
 CREATE TABLE dept_emp (
   emp_no INT NOT NULL,
-  dept_no INT NOT NULL,
+  dept_no VARCHAR(4) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
-);
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+ );
+
 SELECT * FROM departments;
+
+SELECT * FROM employees;
+
+SELECT * FROM salaries;
+
+SELECT * FROM dept_manager;
+
+SELECT * FROM titles;
+
+SELECT * FROM dept_emp;
